4d18308094d5415aca848a854ac3d709ee99bec2
benjamin <halcombbenjamin@gmail.com>
2025-08-01 18:53:50 -0500

    Add @vercel/analytics and @vercel/speed-insights for performance monitoring and analytics integration.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {Home} from "./pages/Home";
 import {NotFound} from "./pages/NotFound";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
+        <Analytics />
+        <SpeedInsights />
     </>
   )
 }
