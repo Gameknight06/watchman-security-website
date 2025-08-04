f66fdc8036321b36b0f7276643e7e96f088c7c78
benjamin <halcombbenjamin@gmail.com>
2025-08-04 16:27:33 -0500

    Remove buttons from HeroSection to simplify design.

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -14,18 +14,6 @@ export const HeroSection = () => {
                         <p className={"text-lg md:text-xl text-center font-semibold max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3"}>
                             Alarm monitoring and technical support for your home or business.
                         </p>
-
-                        <div className={"pt-7 opacity-0 animate-fade-in-delay-4 flex items-center justify-center gap-2 md:gap-4"}>
-                            <a href={"#message"} className={"btn-nm size-2/5 md:size-2/3 bg-gray-300"}>
-                                <span className={"text-xs md:text-2xl"}>Contact Us</span>
-                            </a>
-                            <a href={"#payment"} className={"btn-nm size-2/5 md:size-2/3 bg-gray-300"}>
-                                <span className={"text-xs md:text-2xl"}>Pay A Bill</span>
-                            </a>
-                            <a href={"#about"} className={"btn-nm size-2/5 md:size-2/3 bg-gray-300 "}>
-                                <span className={"text-xs md:text-2xl"}>Learn More</span>
-                            </a>
-                        </div>
                     </div>
                 </div>
         </section>
